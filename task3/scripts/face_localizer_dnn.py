98a65ab9bf6cdb3df78abcce3915cf89a0217221
AljazR <aljoz.rakovec@gmail.com>
2023-05-23 21:05:41 +0200

    Add multiple checkings before posting marker

--- task3/scripts/face_localizer_dnn.py
+++ task3/scripts/face_localizer_dnn.py
@@ -6,26 +6,24 @@ import cv2
 import numpy as np
 import tf2_geometry_msgs
 import tf2_ros
-
 from os.path import dirname, join
-
-#import matplotlib.pyplot as plt
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import PointStamped, Vector3, Pose
 from cv_bridge import CvBridge, CvBridgeError
 from visualization_msgs.msg import Marker, MarkerArray
 from std_msgs.msg import ColorRGBA
 
+global face_poses
 
 class face_localizer:
     def __init__(self):
+        # Initialize the node
         rospy.init_node('face_localizer', anonymous=True)
 
         # An object we use for converting images between ROS format and OpenCV format
         self.bridge = CvBridge()
 
         # The function for performin HOG face detection
-        #self.face_detector = dlib.get_frontal_face_detector()
         protoPath = join(dirname(__file__), "deploy.prototxt.txt")
         modelPath = join(dirname(__file__), "res10_300x300_ssd_iter_140000.caffemodel")
 
@@ -38,21 +36,20 @@ class face_localizer:
         self.marker_array = MarkerArray()
         self.marker_num = 1
 
-        # Subscribe to the image and/or depth topic
-        # self.image_sub = rospy.Subscriber("/camera/rgb/image_raw", Image, self.image_callback)
-        # self.depth_sub = rospy.Subscriber("/camera/depth/image_raw", Image, self.depth_callback)
-
         # Publiser for the visualization markers
-        self.markers_pub = rospy.Publisher('face_markers', MarkerArray, queue_size=1000)
+        self.face_markers_pub = rospy.Publisher('face_markers', MarkerArray, queue_size=1000)
+
+        # Publiser for the poster markers
+        self.poster_markers_pub = rospy.Publisher('poster_markers', MarkerArray, queue_size=1000)
 
         # Object we use for transforming between coordinate frames
         self.tf_buf = tf2_ros.Buffer()
         self.tf_listener = tf2_ros.TransformListener(self.tf_buf)
 
         # Publisher for image of the detected face
-        self.face_pub = rospy.Publisher('face_image', Image, queue_size=1000)
+        self.face_pub = rospy.Publisher('detected_face', Image, queue_size=1000)
 
-    def get_pose(self,coords,dist,stamp):
+    def get_pose(self, coords, dist, stamp):
         # Calculate the position of the detected face
 
         k_f = 554 # kinect focal length in pixels
@@ -66,14 +63,6 @@ class face_localizer:
 
         # Get the angles in the base_link relative coordinate system
         x, y = dist*np.cos(angle_to_target), dist*np.sin(angle_to_target)
-
-        ### Define a stamped message for transformation - directly in "base_link"
-        #point_s = PointStamped()
-        #point_s.point.x = x
-        #point_s.point.y = y
-        #point_s.point.z = 0.3
-        #point_s.header.frame_id = "base_link"
-        #point_s.header.stamp = rospy.Time(0)
 
         # Define a stamped message for transformation - in the "camera rgb frame"
         point_s = PointStamped()
@@ -99,8 +88,6 @@ class face_localizer:
         return pose
 
     def find_faces(self):
-        # print('I got a new image!')
-
         # Get the next rgb and depth images that are posted from the camera
         try:
             rgb_image_message = rospy.wait_for_message("/camera/rgb/image_raw", Image)
@@ -115,7 +102,6 @@ class face_localizer:
             return 0
 
         # Convert the images into a OpenCV (numpy) format
-
         try:
             rgb_image = self.bridge.imgmsg_to_cv2(rgb_image_message, "bgr8")
         except CvBridgeError as e:
@@ -162,31 +148,48 @@ class face_localizer:
                 # Find the location of the detected face
                 pose = self.get_pose((x1,x2,y1,y2), face_distance, depth_time)
 
-                # Check if the face is already detected
+                # if the same pose is detected 3 times in the radius of 0.5m, then it is the same ring
                 new_face = True
+                # if you want to skip the check for the same face, set new_marker to True and comment out the for loop and if statement below
+                new_marker = False
+                for face_pose in face_poses:
+                    if abs(face_pose[0] - pose.position.x) < 0.5 and abs(face_pose[1] - pose.position.y) < 0.5:
+                        new_face = False
+                        if face_poses[face_pose][1] < 3:
+                            face_poses[face_pose][1] += 1
+                            face_poses[face_pose][0].append(pose)
+                            
+                            if face_poses[face_pose][1] == 3:
+                                new_marker = True
+                                # Update the pose with the average of the last 3 poses
+                                pose.position.x = sum([p.position.x for p in face_poses[face_pose][0]]) / 3
+                                pose.position.y = sum([p.position.y for p in face_poses[face_pose][0]]) / 3
+                                pose.position.z = sum([p.position.z for p in face_poses[face_pose][0]]) / 3
+                        break 
+                
+                # If new ring is detected, add it to the dictionary
+                if new_face:
+                    face_poses[(pose.position.x, pose.position.y, pose.position.z)] = [[pose], 1]
+
+
+                # Check if the face is already detected
+                first_in_proximity = True
                 for marker in self.marker_array.markers:
                     if abs(marker.pose.position.x - pose.position.x) < 0.5 and abs(marker.pose.position.y - pose.position.y) < 0.5:
-                        new_face = False
-                        # Update the pose of already detected face
-                        # if face_distance > 1:
-                        # marker.pose = pose
-                        # self.markers_pub.publish(self.marker_array)
+                        first_in_proximity = False
                         print('Face already detected.')
                         break
-                                
-                if pose is not None and new_face:                    
-                    print('Distance to face', face_distance)
-                    print(f'Pose of face [{pose.position.x}, {pose.position.y}]')
+           
+                if pose is not None and new_marker and first_in_proximity:                    
+                    
+                    # TODO: check if it's a poster
                     
                     # Extract region containing face
                     face_region = rgb_image[y1:y2, x1:x2]
 
                     # Publish the image of the face
                     self.face_pub.publish(self.bridge.cv2_to_imgmsg(face_region, "bgr8"))
-
-                    # Visualize the extracted face
-                    # cv2.imshow("ImWindow", face_region)
-                    # cv2.waitKey(1)
+                    print(f'Pose of face [{pose.position.x}, {pose.position.y}] and distance {face_distance}')
 
                     # Create a marker used for visualization
                     self.marker_num += 1
@@ -203,42 +206,24 @@ class face_localizer:
                     marker.color = ColorRGBA(0, 1, 0, 1)
                     self.marker_array.markers.append(marker)
 
-                    self.markers_pub.publish(self.marker_array)
-
-    def depth_callback(self,data):
-
-        try:
-            depth_image = self.bridge.imgmsg_to_cv2(data, "32FC1")
-        except CvBridgeError as e:
-            print(e)
-
-        # Do the necessairy conversion so we can visuzalize it in OpenCV
-        
-        image_1 = depth_image / np.nanmax(depth_image)
-        image_1 = image_1*255
-        
-        image_viz = np.array(image_1, dtype=np.uint8)
-
-        #cv2.imshow("Depth window", image_viz)
-        #cv2.waitKey(1)
-
-        #plt.imshow(depth_image)
-        #plt.show()
+                    self.face_markers_pub.publish(self.marker_array)
+                elif pose is not None and new_marker and not first_in_proximity:
+                    print("Face too close to another face.")
 
 
 def main():
-        # sleep for n seconds so the robot calibrates
-        # rospy.sleep(8)
+    global face_poses
+    face_poses = {}    
+    
+    # Initialize face localizer node
+    face_finder = face_localizer()
 
-        face_finder = face_localizer()
+    rate = rospy.Rate(1)
+    while not rospy.is_shutdown():
+        face_finder.find_faces()
+        rate.sleep()
 
-        rate = rospy.Rate(1)
-        while not rospy.is_shutdown():
-            face_finder.find_faces()
-            rate.sleep()
-
-        cv2.destroyAllWindows()
-
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main()
